ef0d641468b24a94e0c5cb93f55dc7e6df15e92b
root <root@localhost.localdomain>
2020-03-05 09:28:46 +0800

    rewrite cpg2neo4j

--- cpg2neo4j.sh
+++ cpg2neo4j.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
-java -classpath ./lib/* -Dfile.encoding=UTF-8 org.neo4j.batchimport.Importer graph.db nodes.csv rels.csv,cpg_edges.csv
+#patch in the batch_importer_21
+java -classpath "../lib/*" -Dfile.encoding=UTF-8 org.neo4j.batchimport.Importer graph.db nodes.csv rels.csv,cpg_edges.csv
 
+rm -rf /root/graph.db
+mv graph.db /root
+echo "[+] copy right"
+sh restart_neo4j.sh
